fac0e8c203767e1b670924677521a07568127738
hyun dong han <guehgk456@gmail.com>
2020-03-19 18:02:52 +0900

    testing rxbus on MainActivity

--- app/src/main/java/dev/dnights/rxbussample/MainActivity.kt
+++ app/src/main/java/dev/dnights/rxbussample/MainActivity.kt
@@ -2,11 +2,24 @@ package dev.dnights.rxbussample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dev.dnights.rxbussample.rxbus.RxBus
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RxBus.instance.receiveEvent().subscribe({
+            Log.d("test", "it = $it")
+            tv_main.text = it.toString()
+        },{
+            it.printStackTrace()
+        })
+
+        RxBus.instance.sendEvent("123456")
+
     }
 }
