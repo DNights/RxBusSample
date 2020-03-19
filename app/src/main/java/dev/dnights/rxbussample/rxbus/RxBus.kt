d24fa59509bb20791788ec64d8252aebebee5073
hyun dong han <guehgk456@gmail.com>
2020-03-19 18:22:20 +0900

    edited RxBus for multi key

--- app/src/main/java/dev/dnights/rxbussample/rxbus/RxBus.kt
+++ app/src/main/java/dev/dnights/rxbussample/rxbus/RxBus.kt
@@ -1,14 +1,23 @@
 package dev.dnights.rxbussample.rxbus
 
 import io.reactivex.subjects.PublishSubject
+import java.util.*
 
 object RxBus {
 
     val instance = RxBus
+    private val subjectTable = Hashtable<String, PublishSubject<Any>>()
 
-    private var subject: PublishSubject<Any> = PublishSubject.create()
+    fun sendEvent(any: Any, key: String = "RxBus") { subjectTable[key]?.onNext(any) }
 
-    fun sendEvent(any: Any) { subject.onNext(any) }
+    fun receiveEvent(key : String = "RxBus") : PublishSubject<Any>{
+        synchronized(this) {
+            if (subjectTable.containsKey(key).not()) {
+                subjectTable[key] = PublishSubject.create()
+            }
+            return subjectTable[key]!!
+        }
+    }
 
-    fun receiveEvent() = subject
+
 }
