0c91a5ed77af50499bfa6bc87114d67d9ed97c05
hyun dong han <guehgk456@gmail.com>
2020-03-20 09:48:57 +0900

    add BaseActivity.kt

---
+++ app/src/main/java/dev/dnights/rxbussample/BaseActivity.kt
@@ -0,0 +1,13 @@
+package dev.dnights.rxbussample
+
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.disposables.CompositeDisposable
+
+open class BaseActivity : AppCompatActivity() {
+    val compositeDisposable = CompositeDisposable()
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+}
